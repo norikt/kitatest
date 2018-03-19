0a35d1ff38fb956b624716dd834b9e22708da07e
norikt <norikt@gmail.com>
2018-03-19 09:54:16 +0900

    Add turbolinks, rails-ujs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,8 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+import Rails from "rails-ujs";
+import Turbolinks from "turbolinks";
+
+Rails.start();
+Turbolinks.start();
